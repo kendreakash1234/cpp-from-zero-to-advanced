0cc9af8af8fc0a9b745a4cc1d42bd35925018b88
kendreakash1234 <95631192+kendreakash1234@users.noreply.github.com>
2026-08-29 12:42:36 +0000

    Day 0: Add driving eligibility program

--- 00-programming-basics/01-what-is-programming/main.cpp
+++ 00-programming-basics/01-what-is-programming/main.cpp
@@ -7,9 +7,14 @@ int main()
     std::cin>>age;
 
     if(age<18)
+    {
         std::cout<<"You are not eligible to drive."<<std::endl;
+    }
     else
+    {
         std::cout<<"You are eligible to drive."<<std::endl;
+    }
+        
 
     return 0;
 }
